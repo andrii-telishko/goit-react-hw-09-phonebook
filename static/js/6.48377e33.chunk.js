c2af5571354afebb2d7d7dae4a43da3ddeddb831
Andrii Telishko <telishkoav@gmail.com>
2021-08-04 13:25:55 +0300

    Updates

---
+++ static/js/6.48377e33.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcreate-react-app"]=this["webpackJsonpcreate-react-app"]||[]).push([[6],{85:function(e,c,t){},91:function(e,c,t){"use strict";t.r(c),t.d(c,"default",(function(){return a}));t(0),t(85);var n=t(1),a=function(){return Object(n.jsx)("div",{className:"home-container",children:Object(n.jsxs)("h1",{className:"home-title",children:["Welcome to phonebook service"," ",Object(n.jsx)("span",{role:"img","aria-label":"hello icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=6.48377e33.chunk.js.map
